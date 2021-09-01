eedaeccaab74465cb1b9f1e4807615e168560c8c
rakibtweets <rakibhasanroky0@gmail.com>
2021-09-02 05:14:11 +0600

    undefine author and year problem fixed

--- js/app.js
+++ js/app.js
@@ -29,17 +29,25 @@ const searchResultShow = (displayStyle) => {
 const displaySearchResult = books => {
     const booksContainer = document.getElementById('all-books')
     booksContainer.textContent = ''
+    console.log(books)
     books.forEach(book => {
 
         const div = document.createElement('div')
         div.classList.add('col','d-block')
         div.innerHTML = `
-        <div class="card">
-        <img src="https://covers.openlibrary.org/b/id/${book?.cover_i}-M.jpg" class="card-img-top" alt="...">
+        <div class="card border-2 rounded-2 h-100">
+        <img width = 100%; height = "300px" src="https://covers.openlibrary.org/b/id/${book?.cover_i}-M.jpg" class="card-img-top p-2" alt="...">
         <div class="card-body">
-          <h5 class="card-title">${book.title}</h5>
-          <p class="card-text"><small>By</small> ${book.author_name}</p>
-          <p>First Published: ${book.first_publish_year}</p>
+          <h5 class="card-title text-primary">${book.title}</h5>
+          <p class="card-text">
+          Author:
+          <span class="fw-bold">${book.author_name ? book.author_name : 'Author Known'}</span>
+          
+          </p>
+          <p>First Published: 
+          <span class="fw-bold">${book.first_publish_year ? book.first_publish_year : 'Year Unknown' }</span>
+          
+          </p>
 
         </div>
       </div>
