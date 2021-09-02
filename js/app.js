01c3a4de260027e9b970612ec4f6501dd33ed440
rakibtweets <rakibhasanroky0@gmail.com>
2021-09-02 19:59:10 +0600

    Ui error fixed

--- js/app.js
+++ js/app.js
@@ -25,8 +25,6 @@ const toggleSpinner = (displayStyle) => {
 const searchResultShow = (displayStyle) => {
     document.getElementById('result-container').style.display = displayStyle
     document.getElementById('no-result-found').style.display = displayStyle
-
-
 };
 
 // error handling
@@ -34,7 +32,6 @@ const emptySearch = displayStyle => {
     document.getElementById('error').style.display = displayStyle
 
 }
-
 
 // displaying search result
 const displaySearchResult = books => {
@@ -53,7 +50,6 @@ const displaySearchResult = books => {
     if(books.length !== 0){
         document.getElementById('no-result-found').style.display = 'block'
         document.getElementById('no-result-found').innerText = `${books.length} result found`
-        // console.log(books.length)
     };
 
     // loopting through books
@@ -63,23 +59,23 @@ const displaySearchResult = books => {
         div.classList.add('col','d-block')
         div.innerHTML = `
         <div class="card border-2 rounded-2 h-100">
-        <img width = 100%; height = "300px" src="https://covers.openlibrary.org/b/id/${book.cover_i ? book.cover_i: 10909258}-M.jpg" class="card-img-top p-2" alt="...">
+                 <img width = 100%; height = "300px" src="https://covers.openlibrary.org/b/id/${book.cover_i ? book.cover_i: 10909258}-M.jpg" class="card-img-top p-2" alt="...">
         <div class="card-body">
-          <h5 class="card-title text-primary">${book.title}</h5>
-          <p class="card-text">
-          Author:
-          <span class="fw-bold">${book.author_name ? book.author_name : 'Author Known'}</span>
+            <h6 class="card-title text-primary fw-bold">${book.title}</h6>
+            <p class="card-text">
+            Author:
+            <span class="fw-bold">${book.author_name ? book.author_name : 'Author Known'}</span>
           
-          </p>
-          <p class="card-text">
-          Publisher:
-          <span class="fw-bold">${book.publisher ? book.publisher[0]: 'Known publisher'}</span>
+            </p>
+            <p class="card-text">
+            Publisher:
+            <span class="fw-bold">${book.publisher ? book.publisher[0]: 'Known publisher'}</span>
           
-          </p>
-          <p>First Published: 
-          <span class="fw-bold">${book.first_publish_year ? book.first_publish_year : 'Year Unknown' }</span>
+            </p>
+            <p>First Published: 
+            <span class="fw-bold">${book.first_publish_year ? book.first_publish_year : 'Year Unknown' }</span>
           
-          </p>
+            </p>
 
         </div>
       </div>
@@ -92,3 +88,4 @@ const displaySearchResult = books => {
     toggleSpinner('none')
     searchResultShow('block')
 };
+
