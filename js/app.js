29aed4ad03249a45be2b8c96d2469d9b2de2fe4d
rakibtweets <rakibhasanroky0@gmail.com>
2021-09-02 23:35:03 +0600

    checking errors and solved

--- js/app.js
+++ js/app.js
@@ -3,21 +3,26 @@ const loadBooks = () => {
     const searchField = document.getElementById('search-field')
     const bookName = searchField.value 
     searchField.value = ''
+    /* empty search error message */
     if(bookName === ''){
         emptySearch('block')
     }
+    else{
+        emptySearch('none')
+        toggleSpinner('block')
+        searchResultShow('none')
+    
+        // Book search Api call
+        const url = `https://openlibrary.org/search.json?q=${bookName}`
+        fetch(url)
+        .then(res => res.json())
+        .then(data => displaySearchResult(data.docs))
+    }
 
-    toggleSpinner('block')
-    searchResultShow('none')
-
-    // Book search Api call
-    const url = `https://openlibrary.org/search.json?q=${bookName}`
-    fetch(url)
-    .then(res => res.json())
-    .then(data => displaySearchResult(data.docs))
+   
 };
 
-// loader function
+// loader/spinner function
 const toggleSpinner = (displayStyle) => {
     document.getElementById('spinner').style.display = displayStyle
 };
@@ -42,7 +47,6 @@ const displaySearchResult = books => {
     if(books.length === 0){
         document.getElementById('no-result-found').style.display = 'block'
         document.getElementById('no-result-found').innerText = 'No result found'
-        emptySearch('none')
     }
     if(books.length !== 0){
         document.getElementById('no-result-found').style.display = 'block'
@@ -59,19 +63,19 @@ const displaySearchResult = books => {
         <div class="card border-2 rounded-2 h-100">
                  <img width = 100%; height = "300px" src="https://covers.openlibrary.org/b/id/${book.cover_i ? book.cover_i: 10909258}-M.jpg" class="card-img-top p-2" alt="...">
         <div class="card-body">
-            <h5 class="card-title fw-bold">${book.title}</h5>
+            <h5 class="card-title fw-bold text-primary">${book.title}</h5>
             <p class="card-text">
             Author:
-            <span class="fw-bold">${book.author_name ? book.author_name : 'Author Known'}</span>
+            <span class="fw-bold text-primary">${book.author_name ? book.author_name : 'Author Known'}</span>
           
             </p>
             <p class="card-text">
             Publisher:
-            <span class="fw-bold">${book.publisher ? book.publisher[0]: 'Known publisher'}</span>
+            <span class="fw-bold text-primary">${book.publisher ? book.publisher[0]: 'Known publisher'}</span>
           
             </p>
             <p>First Published: 
-            <span class="fw-bold">${book.first_publish_year ? book.first_publish_year : 'Year Unknown' }</span>
+            <span class="fw-bold text-primary">${book.first_publish_year ? book.first_publish_year : 'Year Unknown' }</span>
           
             </p>
 
